bf487f6482e0b76cb886ab0708eaf291d3c9919d
Philip Loa <boyet007@gmail.com>
2019-03-01 11:20:40 +0700

    06 - breakpoints and visibility

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,27 +1,8 @@
 <template>
   <div class="home">
-    <v-btn class="pink white--text">click me</v-btn>
-    <v-btn depressed dark color="blue">click me</v-btn>
-    <v-btn flat color="pink">click me</v-btn>
-  
-    <v-btn depressed class="pink white--text">
-      <v-icon left>email</v-icon> 
-      <span>email me</span>
-    </v-btn>
-
-    <v-btn depressed small class="pink white--text">
-      <v-icon left small>email</v-icon> 
-      <span>email me</span>
-    </v-btn>
-
-    <v-btn depressed large class="pink white--text">
-      <span>email me</span>
-      <v-icon right large>email</v-icon> 
-    </v-btn>
-
-    <v-btn fab depressed small dark color="purple">
-      <v-icon>favorite</v-icon> 
-    </v-btn>
-    
+    <h1>Homepage</h1>
+    <!-- <v-btn class="hidden-md-and-down">click me</v-btn> -->
+    <!-- <v-btn class="hidden-md-and-up">click me</v-btn> -->
+    <v-btn class="hidden-sm-only">clickme</v-btn>
   </div>
 </template>
