a8180c271ac320a48e2e4ce2974a9d1dac7de6f8
Philip Loa <boyet007@gmail.com>
2019-03-01 16:03:00 +0700

    09 - themes update

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -11,11 +11,21 @@
                 <span>Sign Out</span>
                 <v-icon right>exit_to_app</v-icon>
             </v-btn>   
+             </v-toolbar>
 
             <v-navigation-drawer app v-model="drawer"  class="warning" >
-                <p class="success">test</p>
+                <v-list>
+                    <v-list-tile v-for="link in links" :key="link.text" router :to="link.route">
+                        <v-list-tile-action>
+                            <v-icon class="white--text">{{ link.icon }}</v-icon>
+                        </v-list-tile-action>
+                         <v-list-tile-content>
+                            <v-list-tile-title class="white--text">{{ link.text }}</v-list-tile-title>
+                        </v-list-tile-content>
+                    </v-list-tile>
+                </v-list>
             </v-navigation-drawer>
-        </v-toolbar>
+       
     </nav>
 </template>
 
@@ -23,7 +33,12 @@
 export default {
     data() {
         return {
-            drawer: false
+            drawer: false,
+            links: [
+                { icon: 'dashboard', text: 'Dashboard', route: '/'},
+                { icon: 'folder', text: 'My Projects', route: '/projects'},
+                { icon: 'person', text: 'Team', route: '/team'},
+            ]
         }
     }
 }
