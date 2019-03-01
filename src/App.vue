b7c1f74f809a63a377211e552c166d7310fd1aaa
Philip Loa <boyet007@gmail.com>
2019-03-01 11:36:21 +0700

    07 - toolbars

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
-  <v-app>
+  <v-app class="lighten-4 blue">
     <v-content>
+      <NavBar />
       <router-view></router-view>
     </v-content>
   </v-app>
 </template>
 
 <script>
-
+import NavBar from '@/components/NavBar.vue'
 export default {
   name: 'App',
+  components: { NavBar },
   data () {
     return {
       //
